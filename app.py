040aac22c37de9756a02408a31388804c10eecf9
Felipe <pipeag93@gmail.com>
2026-04-28 17:28:59 -0500

    fix: disable Flask reloader causing 502 on Railway

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=port)
